7709536710a854eaa2c4efbf5252b8dd7593614a
Dan Yilmaz <danyilmaz@danyilmaz-ThinkPad-Edge-E530.(none)>
2013-04-23 20:31:16 +0100

    templates added to settings

--- timetabling/views.py
+++ timetabling/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from timetabling.models import Event, Location, Timeslot
+from django.shortcuts import get_object_or_404
+
+
+def getTimeslot(request):
+	timeslots = Timeslot.objects.all()
+	timeslots.order_by('location', 'start_time')
+	return render_to_response('home.htm', {'timeslots' : timeslots})
